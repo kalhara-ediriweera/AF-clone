43aa16d8c79f13c6cf39496092429c9df03d8d87
Kalhara Ediriweera <Kavindukalharaofficial@gmail.com>
2025-05-16 20:51:17 +0530

    done

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+// Initialize the app
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+// Routes
+const authRoutes = require('./routes/authRoutes');
+
+
+app.use('/api/auth', authRoutes);
+
+if(process.env.NODE_ENV !== "production"){
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+}
+// Start the server
+
+export default server;
